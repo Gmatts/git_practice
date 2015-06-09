6df647aa53d7da2d527c1f891ec3b3fd13122f8e
Gmatts <gmatthews@hotchkiss.org>
2015-06-09 14:51:45 +0000

    Added age in dog years

--- git_practice.rb
+++ git_practice.rb
@@ -1,3 +1,8 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hello #{name}!"
+
+puts "What is your age? (numbers only)"
+age = gets.chomp
+dog_age = age.to_i / 7
+puts "If you were a dog, you would be #{dog_age} years old"
